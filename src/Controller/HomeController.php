58b505a3177291fd5ed17a9fb57508e4f383ab18
Jujuslash <juslash@hotmail.fr>
2022-10-16 01:16:27 +0200

    change home

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,18 +5,25 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use src\Service\Requester;
+use Symfony\Component\HttpClient\HttpClient;
 
 class HomeController extends AbstractController
 {
-    #[Route('/home', name: 'app_home')]
+    #[Route('/', name: 'app_home')]
     public function index(): Response
     {
-        $cryptoTab = new RequesterController();
-        $cryptos = $cryptoTab->getMoney();
-
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+        $client = HttpClient::create();
+        $response = $client->request('GET', 'https://pro-api.coinmarketcap.com/v2/cryptocurrency/info?id=1,1027,52', [
+            'headers' => [
+                'content-type' => 'application/json',
+                'X-CMC_PRO_API_KEY'=> '904d0623-35e7-4ef2-8c5a-d4b453c7b78b'
+            ],
+        ]);
+        $cryptos = $response->toArray()["data"];
+        /* return $cryptos;*/
+        return $this->render('requester/index.html.twig', [
+            'cryptostemplate' => $cryptos,   /*var twig = var php*/
+            'developer' => "Julien et Mica"
         ]);
     }
 }
